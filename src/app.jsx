50ac14c6d506c48876977da8803a06d00fa79ba2
E-Hussain06 <ehsanhussain991@gmail.com>
2017-07-25 12:22:50 +0100

    Preparing the Web Request to get Artist data

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
+import accessTokenCode from './access-token';
 import './assets/css/style.css';
 
 class App extends Component {
@@ -13,7 +14,29 @@ class App extends Component {
     }
 
     search() {
-       console.log('this.state', this.state); 
+
+        // Check state
+        console.log('this.state', this.state);
+
+        const BASE_URL = 'https://api.spotify.com/v1/search?';
+        const FETCH_URL = `${BASE_URL}q=${this.state.query}&type=artist&limit=1`;
+        var accessToken = accessTokenCode;
+        var myHeaders = new Headers();
+
+        var myOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + accessToken
+            },
+            mode: 'cors',
+            cache: 'default'
+        };
+
+        fetch(FETCH_URL, myOptions)
+            .then(response => response.json())
+            .then(json => console.log(json))
+
+        console.log('FETCH_URL', FETCH_URL);
     }
 
     render() {
@@ -23,22 +46,22 @@ class App extends Component {
 
                 <FormGroup>
                     <InputGroup>
-                        <FormControl 
-                            type='text' 
-                            placeholder='Search an Artist' 
-                            value={this.state.query} 
-                            onChange={event => {this.setState({query: event.target.value})}}
+                        <FormControl
+                            type='text'
+                            placeholder='Search an Artist'
+                            value={this.state.query}
+                            onChange={event => { this.setState({ query: event.target.value }) }}
                             onKeyPress={event => {
                                 if (event.key === 'Enter') {
                                     this.search();
-                                } 
+                                }
                             }}
                         />
                         <InputGroup.Addon onClick={() => this.search()}>
                             <Glyphicon glyph='search'></Glyphicon>
                         </InputGroup.Addon>
                     </InputGroup>
-                    
+
                 </FormGroup>
 
                 <div className='app-profile'>
