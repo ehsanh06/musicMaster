40479c5f1d1cd65f433277c3d2c8d12ec0c2db4c
E-Hussain06 <ehsanhussain991@gmail.com>
2017-07-31 00:03:54 +0100

    extension to new component added

--- src/profile.component.jsx
+++ src/profile.component.jsx
@@ -3,15 +3,40 @@ import './assets/css/style.css';
 
 class Profile extends Component {
     render() {
-        let artist = {name: '', followers: {total: ''}};
+        let artist = {
+            name: '',
+            followers: {
+                total: ''
+            },
+            images: [{ url: '' }],
+            genres: []
+        };
 
         // Setting artist, in the event that when artist isn't null
         artist = this.props.artist !== null ? this.props.artist : artist;
 
         return (
-            <div>
-                <div>{artist.name}</div>
-                <div>{artist.followers.total}</div>
+            <div className='profile-container'>
+                <img
+                    alt='Profile Image'
+                    className='profile-img'
+                    src={artist.images[0].url}
+                />
+
+                <div className='profile-info'>
+                    <h2 className='profile-artist-name'>{artist.name}</h2>
+                    <p className='profile-artist-followers'>{artist.followers.total} followers</p>
+                    <p className='profile-artist-genres'>
+                        {
+                            artist.genres.map((genre, k) => {
+                                genre = genre !== artist.genres[artist.genres.length - 1] ? ` ${genre},` : ` & ${genre}`;
+                                return (
+                                    <span key={k}>{genre}</span>
+                                )
+                            })
+                        }
+                    </p>
+                </div>
             </div>
         )
     }
