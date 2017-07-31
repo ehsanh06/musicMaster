86f1ff6dbdef88555355aa2bce1edfb89cf2f2b1
E-Hussain06 <ehsanhussain991@gmail.com>
2017-07-31 11:42:15 +0100

    Using JS's Audio functionality to allow user to preview track

--- src/gallery.component.jsx
+++ src/gallery.component.jsx
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './assets/css/style.css';
 
 class Gallery extends Component {
+    playAudio(previewUrl) {
+        let audio = new Audio(previewUrl);
+        audio.play();
+    }
+
     render() {
-        console.log('gallery props', this.props);
         const { tracks } = this.props;
         return (
             <div className='app-gallery'>
                 {tracks.map((track, k) => {
                     const trackImg = track.album.images[0].url;
+                    console.log('Track:', track);
+
                     return (
-                        <div key={k} className='track'>
+                        <div key={k} className='track' onClick={() => this.playAudio(track.preview_url)}>
                             <img src={trackImg} className='track-img' alt='Track Image' />
                             <p className='track-text'>
                                 {track.name}
